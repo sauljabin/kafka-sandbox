fe87ac9d5cc8279045dbec40d8036c570ebb5f3e
Saúl Piña <sauljabin@gmail.com>
2021-07-11 20:52:12 -0500

    Adding mysql and mongo

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,13 @@ val dockerComposePath = "docker-compose"
 val baseCommand = listOf(
     "docker-compose", "-p", project.name,
     "--project-directory", ".",
-    "-f", "./kafka/kafka.yml",
-    "-f", "./kafka/zookeeper.yml",
-    "-f", "./kafka/schema-registry.yml",
-    "-f", "./kafka/akhq.yml",
-    "-f", "./kafka/kafka-connect.yml",
+    "-f", "./docker-compose/kafka.yml",
+    "-f", "./docker-compose/zookeeper.yml",
+    "-f", "./docker-compose/schema-registry.yml",
+    "-f", "./docker-compose/akhq.yml",
+    "-f", "./docker-compose/kafka-connect.yml",
+    "-f", "./docker-compose/mongo.yml",
+    "-f", "./docker-compose/mysql.yml",
 )
 
 task<Exec>("kafkaUp") {
@@ -27,5 +29,17 @@ task<Exec>("kafkaDown") {
 task<Exec>("akhq") {
     description = "Opens AKHQ"
     group = "Kafka Sandbox"
-    commandLine(if (OperatingSystem.current().isLinux()) "xdg-open" else "open", "http://localhost:8080")
+    commandLine(if (OperatingSystem.current().isLinux) "xdg-open" else "open", "http://localhost:8080")
+}
+
+task<Exec>("adminer") {
+    description = "Opens Adminer SQL GUI"
+    group = "Kafka Sandbox"
+    commandLine(if (OperatingSystem.current().isLinux) "xdg-open" else "open", "http://localhost:9090")
+}
+
+task<Exec>("mongoExpess") {
+    description = "Opens Mongo Express GUI"
+    group = "Kafka Sandbox"
+    commandLine(if (OperatingSystem.current().isLinux) "xdg-open" else "open", "http://localhost:7070")
 }
